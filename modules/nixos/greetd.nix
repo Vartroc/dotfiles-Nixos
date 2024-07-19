8623e17b7aefaccf4a990167294b7ebedbb009c3
Vartroc <andi@thurau.net>
2024-07-19 21:09:47 +0200

    test

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -5,7 +5,7 @@ options = {
 	greetd = {
 		enable = lib.mkEnableOption "greetd";
 		command = lib.mkOption {
-			type = lib.typeslib.types..str;
+			type = lib.types.str;
 			default = "Hyprland";
 		};
 	};
