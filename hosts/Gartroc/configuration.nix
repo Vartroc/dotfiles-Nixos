376dbe5339cf89301d858431477f7792b4aca42a
Vartroc <andi@thurau.net>
2024-07-19 21:55:52 +0200

    .

--- hosts/Gartroc/configuration.nix
+++ hosts/Gartroc/configuration.nix
@@ -25,8 +25,8 @@
 		};
 	};
 
-# options.custom.greetd.enable = false;
-# options.custom.getty.enable = true;
+options.greetd.enable = false;
+options.getty.enable = true;
 networking.nameservers = [ "185.222.222.222" "45.11.45.11" ];
   networking.hostName = "Gartroc"; # Define your hostname.
   networking.hostId = "afcefed7";
