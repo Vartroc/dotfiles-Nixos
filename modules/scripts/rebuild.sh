3f5ff3b7b026f8442412c238949633d1a5d8a642
Vartroc <andi@thurau.net>
2024-07-22 22:57:49 +0200

    vorführung julienco

--- modules/scripts/rebuild.sh
+++ modules/scripts/rebuild.sh
@@ -4,22 +4,21 @@ set -e
 echo -e "This script will add everything in /etc/nixos/ to git, commit the changes and rebuild your system\n"
 font="rozzo"
 
-figlet -tc -f $font Testing...
+figlet -tc -f $font Testing... | lolcat
 read -rp "Enter host (default: Gartroc): " host_test
 read -rp "Enter options to pass to the command (default: none): " options_test
 sudo nixos-rebuild test --flake /etc/nixos/#"${host_test}" $options_test
 echo -e "\n"
 
-figlet -tc -f $font Committing...
+figlet -tc -f $font Committing... | lolcat
 cd /etc/nixos/
 sudo git add * 
 read -rp "Enter git commit message: " message
 sudo git -C /etc/nixos commit -m "$message" -a
 echo -e "\n"
 
-figlet -tc -f $font Applying...
+figlet -tc -f $font Applying... | lolcat
 read -rp "Enter host (default: Gartroc): " host
 read -rp "Enter options to pass to the command (default: none): " options
-echo -e "rebuilding... \n\n--------------------Appliing changes on  ${host:-Gartroc} --------------------\n"
 sudo nixos-rebuild switch --flake /etc/nixos/#"${host:-Gartroc}" $options
 
