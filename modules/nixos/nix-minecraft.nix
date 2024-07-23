5e19bc14aa9f42c448808774116ef8c9130c8a89
Vartroc <andi@thurau.net>
2024-07-23 17:00:35 +0200

    nix-minecraft whitelist

--- modules/nixos/nix-minecraft.nix
+++ modules/nixos/nix-minecraft.nix
@@ -15,15 +15,18 @@ services.minecraft-servers = {
 		enable = true;
 		autoStart = true;
 #		restart = "always";
-/*		serverProperties = {
-			
-
+		serverProperties = {
+			white-list = true;
 		};
-*/	
+	
     		package = pkgs.fabricServers.fabric-1_20_1.override { loaderVersion = "0.15.11"; };
     		symlinks = {
       			"mods" = "/home/andi/minecraft-hexamods/mods";
-  		};	
+  		};
+
+		whitelist = {
+			Vartroc = "ce3a67ad-3a84-4bcb-a809-ad9d7330de01";
+		};
 		
 	};
 };
