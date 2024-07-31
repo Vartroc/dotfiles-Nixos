79424581b7cfdd99080e94da7ade14fc80c725af
Vartroc <andi@thurau.net>
2024-07-31 18:59:32 +0200

    nix-minecraft

--- modules/nixos/nix-minecraft.nix
+++ modules/nixos/nix-minecraft.nix
@@ -1,8 +1,8 @@
 { inputs, config, lib, pkgs, ...}:
 let
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/Vartroc/minimal-mods/5afb886a3807e7cb4c499ffb21e0d36c4266c65f/pack.toml";
-    packHash = "vUktLhwlOQL5NhDwavIQ19dcQ+s77LLlNrFa2cayskg=";
+    url = "https://raw.githubusercontent.com/Vartroc/minimal-mods/not_broken/pack.toml";
+    packHash = "gcNhR47IWfrHavuvWmdXsnlRtukSlbreNjM8aYvGoOA=";
   };
 in
 {
