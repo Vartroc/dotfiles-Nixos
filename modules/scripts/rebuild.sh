74c12d8a2c2744b54c792c9042d5fe48a687a71d
Vartroc <andi@thurau.net>
2024-08-07 16:13:11 +0200

    changed rebuild script

--- modules/scripts/rebuild.sh
+++ modules/scripts/rebuild.sh
@@ -5,9 +5,9 @@ echo -e "This script will test your configuration, add everything in /etc/nixos/
 font="rozzo"
 
 figlet -tc -f $font Testing... | lolcat
-read -rp "Enter host (default: Gartroc): " host_test
-read -rp "Enter options to pass to the command (default: none): " options_test
-sudo nixos-rebuild test --flake /etc/nixos/#"${host_test}" $options_test
+read -rp "Enter host (default: Gartroc): " host
+read -rp "Enter options to pass to the command (default: none): " options
+sudo nixos-rebuild test --flake /etc/nixos/#"${host:-Gartroc}" $options
 echo -e "\n"
 
 figlet -tc -f $font Committing... | lolcat
@@ -18,7 +18,5 @@ sudo git -C /etc/nixos commit -m "$message" -a
 echo -e "\n"
 
 figlet -tc -f $font Applying... | lolcat
-read -rp "Enter host (default: Gartroc): " host
-read -rp "Enter options to pass to the command (default: none): " options
 sudo nixos-rebuild switch --flake /etc/nixos/#"${host:-Gartroc}" $options
 
