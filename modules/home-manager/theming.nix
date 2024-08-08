07a7a32fa69ff38a7ab483b239fcad9f8e6a155d
Vartroc <andi@thurau.net>
2024-08-08 13:45:29 +0200

    theming again - attention: --impure used bc of impure path in home-manger/theming.nix

--- modules/home-manager/theming.nix
+++ modules/home-manager/theming.nix
@@ -1,8 +1,19 @@
 { lib, config, pkgs, ... }:
+let 
+	gruvboxplus = import /etc/nixos/modules/home-manager/themes/gruvboxplus.nix { inherit pkgs; };
+in
 {
-gtk.enable = false;
-
-
+gtk = {
+	enable = true;
+	theme = {
+		package = pkgs.adw-gtk3;
+		name = "adw-gtk3";
+	};
+	iconTheme = {
+		package = gruvboxplus;
+		name = "GrubvoxPlus";
+	};
+};
 
 qt = {	
 	enable = true;
