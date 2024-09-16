7581a0e6d1fea5a9e06ab926554604737b86f2f5
Vartroc <andi@thurau.net>
2024-09-16 21:56:59 +0200

    nixvim tectonic compiler

--- modules/nixvim/plugins/vimtex.nix
+++ modules/nixvim/plugins/vimtex.nix
@@ -3,7 +3,10 @@
 programs.nixvim = {
     plugins.vimtex = {
 	enable = true;
-	settings.view_method = "zathura";
+	settings = {
+	  view_method = "zathura";
+	  compiler_method = "tectonic";
+	};
     };
     extraConfigLua = ''
 vim.g.vimtex_mainfile = 0
@@ -12,5 +15,6 @@ vim.g.vimtex_mainfile = 0
 environment.systemPackages = with pkgs; [
     zathura
     biber
+    tectonic
 ];
 }
