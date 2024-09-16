39cb4531b8106a93ac98127434d6126ebd178a59
Vartroc <andi@thurau.net>
2024-09-16 22:19:20 +0200

    remap = bad idea

--- modules/nixvim/keybinds.nix
+++ modules/nixvim/keybinds.nix
@@ -4,47 +4,47 @@ programs.nixvim.keymaps = [
   {
     action = "h";
     key = "b";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "j";
     key = "n";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "k";
     key = "r";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "l";
     key = "s";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "b";
     key = "ä";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "n";
     key = "ö";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "r";
     key = "ü";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "R";
     key = "Ü";
-    options.remap = true;
+    options.noremap = true;
   }
   {
     action = "l";
     key = "ß";
-    options.remap = true;
+    options.noremap = true;
   }
 ];
 }
